7315ed2c895b6acb1ac159a21245b0c04516b9bb
Digvijay Bhakuni <digvijay.bhakuni@rediffmail.com>
2017-04-02 02:28:25 +0530

    correcting the import and provoider for app.com..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { TodosComponent } from './todos/todos.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [TodosComponent]
 })
 export class AppComponent{
   
